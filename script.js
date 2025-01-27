032ace0e0b419505725bbbba62643149397097f2
mvgui <michellyvg@yahoo.com.br>
2025-01-26 21:07:09 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Não modifique as sentenças seguintes
+let numA;
+let numB;
+let numC;
+let numD;
+
+// Modifique aqui os valores!
+numA = 3;
+numB = 3;
+numC = 3;
+numD = 2;
+
+// Não modifique as sentenças seguintes
+
+numA++;
+numC = numB--;
+numC++;
+numA = ++numD;
+console.log("A: " + numA);
+console.log("B: " + numB);
+console.log("C: " + numC);
+console.log("D: " + numD);
+const resultado = numA + numB + numC + numD;
+console.log("Total: " + resultado);
